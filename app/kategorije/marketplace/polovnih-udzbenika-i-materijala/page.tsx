68b10aaa135f3772811a675a24378500d93ddd9c
Amar Tubic <amartubic1@gmail.com>
2025-10-09 11:09:12 +0200

    feat: render book data in PolovneKnjige component

--- app/kategorije/marketplace/polovnih-udzbenika-i-materijala/page.tsx
+++ app/kategorije/marketplace/polovnih-udzbenika-i-materijala/page.tsx
@@ -5,5 +5,19 @@ export default async function PolovneKnjige() {
   const data = await FetchBooks();
   console.log(data);
 
-  return <div>page</div>;
+  return (
+    <div>
+      {" "}
+      {data.map((user, index) => (
+        <ul key={index}>
+          <li>{user.isSold}</li>
+          <li>{user.author}</li>
+          <li>{user.subject}</li>
+          <li>{user.price}</li>
+          <li>{user.createdAt.toLocaleString().slice(0, 10)}</li>
+          <li>{user.id}</li>
+        </ul>
+      ))}
+    </div>
+  );
 }
